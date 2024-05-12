306ce69e945bd8db401ea4031a14f1cd15e3ef6b
sunshine-web-coder <webdev4gaming@gmail.com>
2024-05-12 10:01:24 +0100

    isdone

--- src/pages/resultPage/TrackNo726290/TimeLine726290.jsx
+++ src/pages/resultPage/TrackNo726290/TimeLine726290.jsx
@@ -30,17 +30,17 @@ export default function TimeLine726290() {
                 </time>
               </div>
             </div>
-            <div className="StepProgress-item current">
+            <div className="StepProgress-item is-done">
               <div className="ml-5 h-[70px]">
                 <h3 className="flex items-center mb-1 text-[16px] font-semibold text-[#333333]">
                   OUT FOR DELIVERY
                 </h3>
                 {/* <p className="flex items-center mb-1 text-sm font-semibold text-[#808080]">
                     San Francisco, CA
-                  </p>
+                  </p> */}
                   <time className="block mb-2 text-sm font-normal leading-none text-[#333333] dark:text-gray-500">
-                    October 8th, 2023
-                  </time> */}
+                    May 12, 2023
+                  </time>
               </div>
             </div>
             <div className="StepProgress-item current">
