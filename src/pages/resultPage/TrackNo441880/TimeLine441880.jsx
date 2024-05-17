544d1be2afc908480bfd70c6a596c5ceb50a31b6
sunshine-web-coder <webdev4gaming@gmail.com>
2024-05-17 03:41:35 +0100

    update 441880

--- src/pages/resultPage/TrackNo441880/TimeLine441880.jsx
+++ src/pages/resultPage/TrackNo441880/TimeLine441880.jsx
@@ -43,6 +43,19 @@ export default function TimeLine441880() {
                 </time>
               </div>
             </div>
+            <div className="StepProgress-item is-done">
+              <div className="ml-5 h-[70px]">
+                <h3 className="flex items-center mb-1 text-[16px] font-semibold text-[#333333]">
+                  ON HOLD
+                </h3>
+                {/* <p className="flex items-center mb-1 text-sm font-semibold text-[#808080]">
+                    San Francisco, CA
+                  </p> */}
+                <time className="block mb-2 text-sm font-normal leading-none text-[#333333] dark:text-gray-500">
+                  May 16, 2023
+                </time>
+              </div>
+            </div>
             <div className="StepProgress-item current">
               <div className="ml-5">
                 <h3 className="flex items-center mb-1 text-[16px] font-semibold text-[#333333]">
