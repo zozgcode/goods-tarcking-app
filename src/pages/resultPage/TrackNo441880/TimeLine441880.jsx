a695c0eb0c8a0aa58c6b52261501d94d3ada2f88
sunshine-web-coder <webdev4gaming@gmail.com>
2024-08-23 16:04:56 +0100

    sd

--- src/pages/resultPage/TrackNo441880/TimeLine441880.jsx
+++ src/pages/resultPage/TrackNo441880/TimeLine441880.jsx
@@ -43,7 +43,7 @@ export default function TimeLine441880() {
                 </time>
               </div>
             </div>
-            <div className="StepProgress-item current">
+            <div className="StepProgress-item current hidden">
               <div className="ml-5 h-[70px]">
                 <h3 className="flex items-center mb-1 text-[16px] font-semibold text-[#333333]">
                   ON HOLD
